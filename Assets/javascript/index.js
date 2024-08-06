bbb8dba717da32864b870ac2f06373f57c3f0a67
salvador juarez <101913799+shadownrx@users.noreply.github.com>
2024-08-05 23:40:24 -0300

    Agregada la carpeta Assets

---
+++ Assets/javascript/index.js
@@ -0,0 +1,5 @@
+document.getElementById('login-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    // Aquí puedes agregar la lógica para manejar el inicio de sesión
+    alert('Inicio de sesión enviado');
+  });
